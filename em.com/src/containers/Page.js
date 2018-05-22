98bcd9aa871f4bb6bda381bbf177cf7c517e2169
ericmikkelsen <eric@ericmikkelsen.com>
2018-05-21 21:44:57 -0500

    feat: adds breadcrumbs to Page + Resume.js

--- em.com/src/containers/Page.js
+++ em.com/src/containers/Page.js
@@ -1,16 +1,18 @@
-import React from 'react'
-import { withRouteData } from 'react-static'
 import convert from 'htmr'
-import { Head } from 'react-static'
-//
+import React from 'react'
+import { Head, Link, Router, withRouteData  } from 'react-static'
+
+import { Breadcrumbs } from './components/Breadcrumbs';
 
 export default withRouteData(( {page} ) => (
 
   <article>
       <Head>
+        <meta name="theme-color" content="#325" />
         <title>{page.title}</title>
       </Head>
       <header>
+        <Breadcrumbs breadcrumbs={page.breadcrumbs} />
         <h1>{page.title}</h1>
       </header>
       {convert(page.contents)}
--- em.com/src/containers/Resume.js
+++ em.com/src/containers/Resume.js
@@ -2,14 +2,17 @@
 import React from 'react'
 import { withRouteData } from 'react-static'
 import convert from 'htmr'
-//
+import {Breadcrumbs} from './components/Breadcrumbs';
+import {WorkHistory} from './components/WorkHistory';
 
 export default withRouteData(({ resume }) => (
   <article>
+    <Breadcrumbs breadcrumbs={resume.breadcrumbs} />
+
     <h1 id="content" className="dn-p">{resume.title}</h1>
-    <div className="header--print dn-s">
-      <h1 >Eric Mikkelsen</h1>
-    </div>
+
+    <h1 className="header--print dn-s">Eric Mikkelsen</h1>
+
     {convert(resume.contents)}
     <h2>Skills</h2>
     <ul className="skills">
@@ -23,33 +26,6 @@ export default withRouteData(({ resume }) => (
             </li>
       })}
     </ul>
-    <section className="break-avoid dib">
-      <h2>Work History</h2>
-      {resume.workHistory.map(function(job, i){
-          return <div
-              className="job"
-              id={job.i}
-              itemScope 
-              itemType="http://schema.org/Organization"
-              key={i} 
-            >
-              <h3 itemProp="name">
-                {job.organization}
-              </h3>
-              <p itemProp="jobTitle">
-                <strong>{job.jobTitle}</strong>
-              </p>
-              <time itemProp="jobTitle">
-                {job.date}
-              </time>
-              <p
-                className="job__description" 
-                itemProp="description"
-              >
-                {job.description}
-              </p>
-            </div>
-      })}
-    </section>
+    <WorkHistory workHistory={resume.workHistory} />
   </article>
 ))
