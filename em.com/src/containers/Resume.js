6cde8897e26a96d808451ba13291227a871f203b
ericmikkelsen <eric@ericmikkelsen.com>
2018-08-23 16:39:59 -0700

    feat: adds resume title tag

--- em.com/src/containers/Resume.js
+++ em.com/src/containers/Resume.js
@@ -1,14 +1,16 @@
 
 import React from 'react'
-import { withRouteData } from 'react-static'
+import { withRouteData, Head } from 'react-static'
 import convert from 'htmr'
 import {Breadcrumbs} from './components/Breadcrumbs';
 import {WorkHistory} from './components/WorkHistory';
 
 export default withRouteData(({ resume }) => (
   <article>
+    <Head>
+      <title>Resume</title>
+    </Head>
     <Breadcrumbs breadcrumbs={resume.breadcrumbs} />
-
     <h1 className="header--print dn-s">Eric Mikkelsen</h1>
 
     <h1 id="content" className="dn-p">{resume.title}</h1>
