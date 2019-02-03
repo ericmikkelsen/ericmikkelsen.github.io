aeaae4b43c53d5a9001baf29c14ba63a72c9a900
ercimikkelsen <eric@ericmikkelsen.com>
2019-02-03 21:54:18 +0000

    feat: small updates to content

---
+++ react-static-browser-plugins.js
@@ -0,0 +1,12 @@
+// Imports
+
+
+// Plugins
+const plugins = [{
+  location: "/home/irksum/projects/ericmikkelsen.github.io/em.com",
+  plugins: [],
+  hooks: {}
+}]
+
+// Export em!
+export default plugins
